d7abfb65083e8dcf17d87d1ea6829d1a86b849bd
Vishnu Prasad N <vishnuprasad91332@gmail.com>
2025-03-08 22:11:35 +0530

    package update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'taskmanager-ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('taskmanager-ui');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, taskmanager-ui');
-  });
 });
