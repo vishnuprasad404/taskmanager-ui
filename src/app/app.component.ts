36b97601d55bddb8afb3e652dacd2b3e435b5983
unknown <20br13587@rit.ac.com>
2025-03-08 19:29:10 +0530

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,6 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'taskmanager-ui';
-}
+export class AppComponent {}
