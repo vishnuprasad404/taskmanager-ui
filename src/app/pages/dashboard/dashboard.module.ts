5af03937ade31cc43c1bda6824d462bb25adf7e0
Vishnu Prasad N <vishnuprasad91332@gmail.com>
2025-03-09 21:49:20 +0530

    sidebar component added

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashborad.route';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
