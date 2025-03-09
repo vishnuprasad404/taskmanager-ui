b8114e78959966a195f55f631a0c9136562541f2
Vishnu Prasad N <vishnuprasad91332@gmail.com>
2025-03-09 10:30:23 +0530

    module file added

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes,{ useHash: true})
+  ]
+})
+export class AppModule { }
